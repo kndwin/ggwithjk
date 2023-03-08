2fdb1198348abb9ba42d3db895bf04af00822df1
kndwin <tnkevino@gmail.com>
2023-03-08 18:47:41 +1100

    feat: added term 1, week 4 content

---
+++ utils/profiles.tsx
@@ -0,0 +1,24 @@
+import * as SVG from "~/svgs";
+
+export const profiles = [
+  {
+    name: "Jaison",
+    imageSrc: "/jaison.jpeg",
+    svg: <SVG.Jaison className="h-full w-full" />,
+    description: `Jaison was born in India but grew up in Sydney. He grew up in a Christian family but only became a Christian himself when he discovered the Bible’s radical message about the grace of God while listening to a sermon during his lunch break at work. Jaison now loves seeing others from all different backgrounds and age-groups learning about this same grace shown to sinners in the death and resurrection of Christ. He enjoys having a good cold brew coffee with friends and loves watching Marvel movies.
+
+    		Jaison oversees the Engage Youth Ministry, pastors the 5pm congregation and leads the Explore Evangelistic Ministry.`,
+  },
+  {
+    name: "Chi Chi",
+    imageSrc: "/chichi.jpeg",
+    svg: <SVG.ChiChi className="h-full w-full" />,
+    description: `Consectetur sunt minima nemo nulla obcaecati Molestiae eligendi eligendi blanditiis ipsa minima, quas blanditiis Deleniti non eius animi exercitationem sint Quaerat distinctio eaque consequatur nam illo Atque unde adipisci repellendus.`,
+  },
+  {
+    name: "Kevin",
+    imageSrc: "/kevin.jpeg",
+    svg: <SVG.Kevin className="h-full w-full" />,
+    description: `Kevin is a handsome young man`,
+  },
+];
